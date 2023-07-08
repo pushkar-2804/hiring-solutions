260eb2604919b6997b55c83deb49f226fec0c28e
pushkar-28 <kharepushkar2804@gmail.com>
2023-07-08 23:29:44 +0530

    feature and benefit sections

--- script.js
+++ script.js
@@ -21,3 +21,29 @@ document.addEventListener("DOMContentLoaded", function () {
     });
   }
 });
+
+document.addEventListener("DOMContentLoaded", function () {
+  var hamburgerMenu = document.querySelector(".hamburger-menu");
+  var navbarLinks = document.querySelector(".navbar-links");
+
+  hamburgerMenu.addEventListener("click", function () {
+    hamburgerMenu.classList.toggle("change");
+    navbarLinks.classList.toggle("change");
+  });
+});
+
+function reveal() {
+  var reveals = document.querySelectorAll(".reveal");
+  for (var i = 0; i < reveals.length; i++) {
+    var windowHeight = window.innerHeight;
+    var elementTop = reveals[i].getBoundingClientRect().top;
+    var elementVisible = 150;
+    if (elementTop < windowHeight - elementVisible) {
+      reveals[i].classList.add("active");
+    } else {
+      reveals[i].classList.remove("active");
+    }
+  }
+}
+window.addEventListener("scroll", reveal);
+reveal();
