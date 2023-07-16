449e3ec1a0692a3deec4cbd45cff41cb398d4841
pushkar-28 <kharepushkar2804@gmail.com>
2023-07-16 16:26:02 +0530

    post candidate

--- server/controllers/candidateController.js
+++ server/controllers/candidateController.js
@@ -32,7 +32,30 @@ const getAllCandidates = async (req, res) => {
   }
 };
 
+// Candidate creation endpoint
+const createNewCandidate = async (req, res) => {
+  try {
+    const { name, email, location, jobRole } = req.body;
+
+    // Create a new candidate instance
+    const newCandidate = new Candidate({
+      name,
+      email,
+      location,
+      jobRole,
+    });
+
+    // Save the new candidate in the database
+    const createdCandidate = await newCandidate.save();
+
+    res.status(201).json({ candidate: createdCandidate });
+  } catch (error) {
+    res.status(500).json({ message: "Internal server error" });
+  }
+};
+
 module.exports = {
   searchCandidate,
   getAllCandidates,
+  createNewCandidate,
 };
--- server/routes/candidateRoutes.js
+++ server/routes/candidateRoutes.js
@@ -4,11 +4,13 @@ const authMiddleware = require("../middlewares/authMiddleware");
 const {
   searchCandidate,
   getAllCandidates,
+  createNewCandidate,
 } = require("../controllers/candidateController");
 
 const router = express.Router();
 
 router.post("/search", authMiddleware, searchCandidate);
+router.post("/create-candidate", authMiddleware, createNewCandidate);
 router.get("/all", authMiddleware, getAllCandidates);
 
 module.exports = router;
